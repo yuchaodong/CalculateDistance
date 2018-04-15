2b03fa599839c49e9bb89dbf21edc9a0de95a2fc
YuChao Dong <yuchaodong@gmail.com>
2018-04-15 18:27:32 -0400

    cleaned up syntax

--- public/index.js
+++ public/index.js
@@ -1,20 +1,21 @@
-function getFromAirport() {
+function getAirports() {
+    const airport1 = document.getElementById('airport1').value;
+    const airport2 = document.getElementById('airport2').value;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        // Typical action to be performed when the document is ready:
-        document.getElementById("displayDistance").innerHTML = xhttp.responseText;
+        document.getElementById('displayDistance').innerHTML = xhttp.responseText;
       }
     };
     var url = '/distance'
-    xhttp.open("POST", url, true);
+    xhttp.open('POST', url, true);
     xhttp.setRequestHeader('Content-Type', 'application/json')
     xhttp.send(JSON.stringify({
-        'airport1': 'ABE',
-        'airport2': 'ABI'
+        'airport1': airport1,
+        'airport2': airport2
     }));
 }
 
 
 const getAirportCode = document.getElementById('submit');
-getAirportCode.addEventListener("click", getFromAirport);
+getAirportCode.addEventListener('click', getAirports);
--- server/server.js
+++ server/server.js
@@ -3,14 +3,12 @@ const bodyParser = require('body-parser');
 const getDistanceBetweenAirports = require('./getDistanceBetweenAirports.js');
 const app = express();
 
-app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.get('/test', function(req, res) {
-    res.send('hello00000')
-})
+app.use(express.static('public'));
+
 
 app.post('/distance', function(req, res) {
     const distance = getDistanceBetweenAirports(req.body.airport1, req.body.airport2);
