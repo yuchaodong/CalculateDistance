a3ad32ec12d490de929fe3ae40c461b75420a36b
YuChao Dong <yuchaodong@gmail.com>
2018-04-15 18:54:11 -0400

    added error info on the front-end

--- public/index.js
+++ public/index.js
@@ -3,9 +3,14 @@ function getAirports() {
     const airport2 = document.getElementById('airport2').value;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('displayDistance').innerHTML = xhttp.responseText;
-      }
+        const display = document.getElementById('displayDistance')
+        if (this.readyState === 4 && this.status === 200) {
+           display.innerHTML = xhttp.responseText;
+        } else if (this.status === 500) {
+            display.innerHTML = 'Your inputs are invalid';
+        } else {
+            display.innerHTML = `Uncertain error: ${this.status}`;         
+        }
     };
     var url = '/distance'
     xhttp.open('POST', url, true);
