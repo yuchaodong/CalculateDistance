c90c17ee07e9eae364091bc792246b7c722e07c8
YuChao Dong <yuchaodong@gmail.com>
2018-04-16 16:22:10 -0400

    working on incorporating google maps api

--- public/index.js
+++ public/index.js
@@ -1,3 +1,25 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 3,
+      center: {lat: 0, lng: -180},
+      mapTypeId: 'terrain'
+    });
+
+    var flightPlanCoordinates = [
+        {lat: 37.772, lng: -122.214},
+        {lat: 21.291, lng: -157.821},
+        {lat: -18.142, lng: 178.431},
+        {lat: -27.467, lng: 153.027}
+      ];
+      var flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+}
+
 function getAirports() {
     const airport1 = document.getElementById('airport1').value.toUpperCase();
     const airport2 = document.getElementById('airport2').value.toUpperCase();
@@ -26,3 +48,6 @@ function getAirports() {
 
 const getDistance = document.getElementById('calculateDistanceButton');
 getDistance.addEventListener('click', getAirports);
+
+const displayMap = document.getElementById('map');
+displayMap.innerHTML= `${initMap()}`;
